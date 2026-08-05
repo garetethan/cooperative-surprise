56bc58400a34f05f46db38769854d6f8fde3ca32
garetethan <15016851+garetethan@users.noreply.github.com>
2026-08-05 15:34:23 -0700

    Keep user registration view dynamic after error
    Previously JavaScript baked into the user registration view dynamically showed and hid fields depending on earlier inputs.
    But when invalid values were submitted (and they were caught at a high enough level), the page appeared mostly the same, but the JavaScript stopped working.
    Wrap it in event listeners so that it continues working, and also move it to a stand-alone JavaScript file.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,27 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+['turbo:load', 'turbo:render'].forEach(turbo_event => {
+	document.addEventListener(turbo_event, function() {
+		// User registration page
+		// Hide the family ID field if creating a new family
+		// Hide the family name field if joining an existing family
+		let familyCodeField = document.getElementById('family-code-field');
+		let familyNameField = document.getElementById('family-name-field');
+
+		if (familyCodeField && familyNameField) {
+			let newFamilyTriggers = document.querySelector('[data-action="new-family"]')
+			newFamilyTriggers.addEventListener('click', function() {
+			  familyCodeField.style.display = 'none';
+			  familyNameField.style.display = 'block';
+			});
+
+			let existingFamilyTriggers = document.querySelector('[data-action="existing-family"]')
+			existingFamilyTriggers.addEventListener('click', function() {
+			  familyCodeField.style.display = 'block';
+			  familyNameField.style.display = 'none';
+			});
+		}
+	});
+});
