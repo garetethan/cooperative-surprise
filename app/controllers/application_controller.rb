4ef5cbae0431748df94d36c0f527bb71a5579c5c
garetethan <15016851+garetethan@users.noreply.github.com>
2026-08-06 18:42:17 -0700

    Merge Home controller into Application controller
    Remove the Home controller, which only managed the home page.
    Add a method for the home page to the existing Application controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,9 @@ class ApplicationController < ActionController::Base
   allow_browser versions: :modern
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def index
+  end
+
   protected
 
   def configure_permitted_parameters
